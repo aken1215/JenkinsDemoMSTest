3398a565fc037d2aadcf699980fb14f3914a1799
Hank <aken1215@hotmail.com>
2016-10-20 16:39:38 +0800

    上傳

---
+++ TestMsBuild/TestMsBuild/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMsBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Test";
+
+            string b = "";
+            if (a == "Test")
+            {
+                b = "Test";
+            }
+            else if(a=="Test1")
+            {
+                b = "Test1";
+            }
+
+            SetStatus(b);
+        }
+
+        private static void SetStatus(string b)
+        {
+
+        }
+    }
+}
