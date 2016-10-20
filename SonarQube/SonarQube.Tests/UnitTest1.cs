e1c06c0f8c28166b0ded1cdd3e0df03ac4c5ee26
Hank <aken1215@hotmail.com>
2016-10-20 16:45:00 +0800

    first commit

---
+++ SonarQube/SonarQube.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SonarQube.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Calculate()
+        {
+            var target = new Calculate();
+            var first = 1;
+            var second = 2;
+
+            var actual = target.plus(first, second);
+            var expected = 3;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ SonarQube/SonarQube/Calculate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SonarQube
+{
+    public class Calculate
+    {
+        public int plus(int first ,int second)
+        {
+            return first + second;
+        }
+    }
+}
